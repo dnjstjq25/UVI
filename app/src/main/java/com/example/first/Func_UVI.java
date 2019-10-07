d096ef59e5f0913523763376d027bc9b9faf32c0
hozero95 <45387924+hozero95@users.noreply.github.com>
2019-10-08 01:40:06 +0900

    1. 코드 줄맞춤

--- app/src/main/java/com/example/first/Func_UVI.java
+++ app/src/main/java/com/example/first/Func_UVI.java
@@ -11,8 +11,8 @@ import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 public class Func_UVI {
-    public String Test(Activity activity){
-        int[][] input = new int[][]{{3},{7}};
+    public String Test(Activity activity) {
+        int[][] input = new int[][]{{3}, {7}};
         int[] output = new int[]{0};
 
         Interpreter tflite = getTfliteInterpreter(activity, "test.tflite");
@@ -21,7 +21,7 @@ public class Func_UVI {
         return String.valueOf(output[0]);
     }
 
-    public String Output(Activity activity, float solarZenith, float illumination){
+    public String Output(Activity activity, float solarZenith, float illumination) {
 //        float[] input = new float[]{solarZenith, illumination};
 //        float[] output = new float[]{0};
 //
@@ -47,9 +47,9 @@ public class Func_UVI {
 
     // 모델 파일 인터프리터를 생성하는 함수
     private Interpreter getTfliteInterpreter(Activity activity, String modelPath) {
-        try{
+        try {
             return new Interpreter(loadModelFile(activity, modelPath));
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
@@ -57,7 +57,7 @@ public class Func_UVI {
 
     // 모델을 읽어오는 함수 (텐서플로 라이트 홈페이지 참고)
     // MappedByteBuffer 바이트 버퍼를 Interpreter 객체에 전달하면 모델 해석을 할 수 있다.
-    private MappedByteBuffer loadModelFile(Activity activity, String modelPath) throws IOException{
+    private MappedByteBuffer loadModelFile(Activity activity, String modelPath) throws IOException {
         AssetFileDescriptor fileDescriptor = activity.getAssets().openFd(modelPath);
         FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
         FileChannel fileChannel = inputStream.getChannel();
