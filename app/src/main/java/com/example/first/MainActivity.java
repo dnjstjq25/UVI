f5325a8659a84067e49a9bbf269b44bc79945779
dnjstjq25 <dnjstjq25@naver.com>
2019-10-08 14:33:40 +0900

    OkHttp_code.java 삭제
    코드내용 MainActivity로 옮김

--- app/src/main/java/com/example/first/MainActivity.java
+++ app/src/main/java/com/example/first/MainActivity.java
@@ -16,6 +16,17 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private SensorManager sensorManager;
@@ -47,6 +58,12 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private double longitude;
     private double solarZenith;
     private String uvi;
+
+    //OkHttp 관련 선언
+    OkHttpClient client = new OkHttpClient();
+    Request request = new Request.Builder()
+            .url("http://210.102.142.16/model.tflite")
+            .build();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -163,6 +180,98 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         if (isAccessFineLocation && isAccessCoarseLocation) {
             isPermission = true;
+        }
+    }
+
+    //OkHttp 코드
+    private class CallbackToDownloadFile implements Callback {
+
+        MainActivity.CallbackToDownloadFile cbToDownloadFile = new MainActivity.CallbackToDownloadFile(
+                "/storage/extSdCard/Android/data/cf.domone.android.example",
+                "file_name_example"
+
+        );
+
+
+        private File directory;
+        private File fileToBeDownloaded;
+
+        public CallbackToDownloadFile(String directory, String fileName) {
+            this.directory = new File(directory);
+            this.fileToBeDownloaded = new File(this.directory.getAbsolutePath() + "/" + fileName);
+        }
+
+        @Override
+        public void onFailure(Request request, IOException e) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(
+                            MainActivity.this,
+                            "파일을 다운로드할 수 없습니다. 인터넷 연결을 확인하세요.",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            });
+        }
+
+        @Override
+        public void onResponse(Response response) throws IOException {
+            if (!this.directory.exists()) {
+                this.directory.mkdirs();
+            }
+
+            if (this.fileToBeDownloaded.exists()) {
+                this.fileToBeDownloaded.delete();
+            }
+
+            try {
+                this.fileToBeDownloaded.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        Toast.makeText(
+                                MainActivity.this,
+                                "다운로드 파일을 생성할 수 없습니다.",
+                                Toast.LENGTH_SHORT
+                        ).show();
+                    }
+                });
+
+                return;
+            }
+
+            InputStream is = response.body().byteStream();
+            OutputStream os = new FileOutputStream(this.fileToBeDownloaded);
+
+            final int BUFFER_SIZE = 2048;
+            byte[] data = new byte[BUFFER_SIZE];
+
+            int count;
+            long total = 0;
+
+            while ((count = is.read(data)) != -1) {
+                total += count;
+                os.write(data, 0, count);
+            }
+
+            os.flush();
+            os.close();
+            is.close();
+
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(
+                            MainActivity.this,
+                            "다운로드가 완료되었습니다.",
+                            Toast.LENGTH_SHORT
+                    ).show();
+                }
+            });
         }
     }
 }
